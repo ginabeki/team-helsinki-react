e19849bdb267a43d8c65fe93b501d41fb677a7d8
Joseph Maramba <joseph@smartmile.eu>
2022-10-21 05:44:08 +0300

    extending cra eslint config & testing lint scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,16 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
-  extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime', 'airbnb'],
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'airbnb',
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
@@ -13,12 +21,5 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/react-in-jsx-scope': 'off',
-    'react/function-component-definition': [
-      2,
-      {
-        namedComponents:
-          'function-declaration' | 'function-expression' | 'arrow-function',
-      },
-    ],
   },
 };
