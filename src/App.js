89e2a7c107c5ea6c50c1652d938e72fd42549e48
Joseph Maramba <joseph@smartmile.eu>
2022-10-21 07:38:26 +0300

    testing pre-commit config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import "./App.scss";
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Team Helsinki Hello World</p>
-      <p>Sass works!</p>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <p>Team Helsinki Hello World</p>
+    <p>Sass works!</p>
+  </div>
+);
 
 export default App;
