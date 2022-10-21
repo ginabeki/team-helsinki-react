86930d985912c95d72ad18c3dfad873835cccdbc
Joseph Maramba <joseph@smartmile.eu>
2022-10-21 07:40:43 +0300

    testing pre-commit config with eslint/prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.scss';
+import "./App.scss";
 
-const App = () => (
-  <div className="App">
-    <p>Team Helsinki Hello World</p>
-    <p>Sass works!</p>
-  </div>
-);
+function App() {
+  return (
+    <div className="App">
+      <p>Team Helsinki Hello World</p>
+      <p>Sass works!</p>
+    </div>
+  );
+}
 
 export default App;
