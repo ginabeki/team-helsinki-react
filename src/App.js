3e66d97075c03aa0c5a15158292128cb6b648d03
ginabeki <ginabeki3@gmail.com>
2022-10-27 23:36:35 +0300

    Update navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import "./App.scss";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Event from "./pages/Event";
+import Job from "./pages/Job";
+import Forum from "./pages/Forum";
+// import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <p>Team Helsinki Hello World</p>
-      <p>Sass works!</p>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/job" element={<Job />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/forum" element={<Forum />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,44 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+// import Button from "./components/Button";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="logo">
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/logo.svg`}
+          alt="Organization logo"
+        />
+      </Link>
+      <ul className="menu-item">
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/about">About us</CustomLink>
+        <CustomLink to="/forum">Forum</CustomLink>
+        <CustomLink to="/event">Event</CustomLink>
+        <CustomLink to="/blog">Blog</CustomLink>
+        <CustomLink to="/job">Job</CustomLink>
+      </ul>
+      <div className="buttons">
+        <Button type="submit"> Login</Button>
+        <Button type="submit">Signin</Button>
+      </div>
+    </nav>
+  );
+}
+
+// eslint-disable-next-line react/prop-types
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({
+    path: resolvedPath.pathname,
+    end: true,
+  });
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
